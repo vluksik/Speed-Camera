256959650f3ba935d771439556f1ee9daf509f58
vluksik@gmail.com <vluksik@gmail.com>
2021-01-23 21:43:43 +0100

    better

---
+++ Speed Camera/maxSpeed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Speed_Camera
+{
+    public class maxSpeed
+    {
+        public int speedLimit;
+        public int actualSpeed;
+
+        public void enterSpeed()
+        {
+            Console.WriteLine("Enter speed: " );
+        }
+    }
+}
